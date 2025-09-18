0b3ef3fae7f05a5099dc2b8eb308ef01d741be97
ajasma6570 <ajasmaprince@gmail.com>
2025-09-19 00:42:32 +0530

    refactor: hero page intro gif removed

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -7,8 +7,8 @@ import { Icon } from "@iconify/react/dist/offline";
 import bag4Linear from "@iconify/icons-solar/bag-4-linear";
 import arrowRight from "@iconify/icons-lucide/arrow-right";
 import Image from "next/image";
-import IntroDesktop from "$/gif/intro-desktop.gif";
-import IntroMobile from "$/gif/intro-mobile.gif";
+import IntroDesktop from "$/images/intro.jpg";
+import IntroMobile from "$/images/intro_mobile.jpg";
 
 export default function Hero() {
   return (
