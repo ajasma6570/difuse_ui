252ad6588c3fbedd172374f5ce2340caa0cb1bc2
ajasma6570 <ajasmaprince@gmail.com>
2025-10-05 19:46:16 +0530

    refcator: 404 page updated

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,16 +6,38 @@ import Footer from "@/lib/components/common/Footer";
 export default function NotFound() {
   return (
     <main>
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-7xl font-bold text-[#1C1E55] mb-4">404</h1>
-        <p className="text-lg mb-4">Page Not Found</p>
-        <AnimatedSlideButton
-          icon={<Icon icon={arrowRight} width={24} height={24} />}
-          text="Go to Home"
-          className=" items-center gap-2 text-white bg-[#1C1E55] hover:bg-[#151347] transition-colors p-3 rounded-lg text-xl tracking-[-0.05em] max-w-max md:inline-flex"
-          variant="link"
-          href="/"
-        />
+      <div className="text-center min-h-screen flex flex-col items-center justify-center px-6 space-y-6">
+        <div>
+          <div className="relative">
+            <h1 className="text-9xl md:text-[200px] leading-none">
+              <span className="text-[#080808]">4</span>
+              <span className="text-[#25276C]">0</span>
+              <span className="text-[#080808]">4</span>
+            </h1>
+            <p className="absolute top-3 md:top-4 right-5 md:right-12 left-1/2 text-[#25276C] text-lg md:text-xl">
+              ops!
+            </p>
+          </div>
+
+          <p className="text-lg md:text-xl font-normal text-[#2A2A2A]">
+            Page not found
+          </p>
+        </div>
+
+        <div className="mt-4 flex flex-col md:flex-row items-center bg-[#F4F4F4] p-8 w-full md:w-3xl justify-between gap-8 rounded-lg">
+          <p className="text-lg text-[#2A2A2A] max-w-sm text-start">
+            The page you are looking for doesn’t exist or another error
+            occurred.
+          </p>
+
+          <AnimatedSlideButton
+            icon={<Icon icon={arrowRight} width={24} height={24} />}
+            text="Back to Homepage"
+            className="inline-flex text-[#1C1E55] hover:bg-[#1C1E55] border border-[#1C1E55] hover:text-white rounded-lg bg-[#F4F4F4] text-xl tracking-[-0.05em] p-5 max-w-max "
+            variant="link"
+            href="/"
+          />
+        </div>
       </div>
       <div className="bg-[linear-gradient(to_bottom,black_10%,#25276C_100%)] w-full mt-5">
         <Footer />
