41ca948ea73e592238344c2ce0095940ab087fc1
ajasma6570 <ajasmaprince@gmail.com>
2025-10-25 18:19:11 +0530

    feat:next config added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: 'build',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  webpack: (
+    config,
+    { dev }
+  ) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+    }
+    return config
+  },
 };
 
 export default nextConfig;
