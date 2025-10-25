2459b71bd4ad79d871461d9ec9e3b96a2dbea506
ajasma6570 <ajasmaprince@gmail.com>
2025-10-25 18:37:25 +0530

    fix: scroll issue

--- src/lib/components/common/SmoothScroll.tsx
+++ src/lib/components/common/SmoothScroll.tsx
@@ -1,18 +1,50 @@
 "use client";
-import { useEffect } from "react";
-import Lenis from "lenis";
+import { ReactLenis, useLenis } from "lenis/react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { ReactNode, useEffect } from "react";
 
-export default function SmoothScroll() {
-  useEffect(() => {
-    const lenis = new Lenis();
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
-
-  return null;
+interface SmoothScrollingProps {
+  children: ReactNode;
 }
+
+const SmoothScrollProvider = ({ children }: Readonly<SmoothScrollingProps>) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const lenis = useLenis();
+
+  useEffect(() => {
+    lenis?.scrollTo(0, { immediate: true });
+  }, [pathname, searchParams, lenis]);
+
+  useEffect(() => {
+    if (!lenis) return;
+
+    const handleClick = (ele: Element) => {
+      lenis.scrollTo(ele.getAttribute("href") ?? "", {
+        offset: -100,
+      });
+    };
+
+    const elements = document.querySelectorAll(".lenis-scroll-to");
+    const clickHandler = (e: Event) => handleClick(e.target as Element);
+
+    elements.forEach((ele) => {
+      ele.addEventListener("click", clickHandler);
+    });
+
+    return () => {
+      elements.forEach((ele) => {
+        ele.removeEventListener("click", clickHandler);
+      });
+    };
+  }, [lenis, pathname]);
+
+  return (
+    <ReactLenis root options={{ duration: 1.1 }}>
+      {children}
+    </ReactLenis>
+  );
+};
+
+export default SmoothScrollProvider;
